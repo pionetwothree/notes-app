4e3c44b5c6db09c2d9a602c813007b86552e47d8
pionetwothree <75122491+pionetwothree@users.noreply.github.com>
2021-02-27 21:48:19 +0100

    Add files via upload

---
+++ routes/noteRoute.js
@@ -0,0 +1,23 @@
+const express = require("express");
+//const { readdirSync } = require("fs");
+const router = express.Router();
+const Note = require("../models/noteModel");
+
+router.route("/create").post((req, res) => {
+    const title = req.body.title;
+    const content = req.body.content;
+    const newNote = new Note({
+        //_id,
+        title, 
+        content
+    });
+
+    newNote.save();
+})
+
+router.route("/notes").get((req, res) => {
+    Note.find()
+        .then(foundNotes => res.json(foundNotes))
+})
+
+module.exports = router;

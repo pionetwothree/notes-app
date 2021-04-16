0ba67cb0e2754a730f6d97f735e455aa6259ad0b
pionetwothree <75122491+pionetwothree@users.noreply.github.com>
2021-04-16 14:15:02 +0200

    Add files via upload

---
+++ frontend/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const mongoose = require('mongoose');
+const console = require('console');
+
+app.use(cors());
+app.use(express.json());
+
+//connect to mongoose
+mongoose.connect('mongodb+srv://Dom:RjIbzYxjHaL8ypxa@cluster0.fpxgk.mongodb.net/Test')
+
+//require route
+app.use("/", require("./routes/noteRoute"));
+
+app.get("/create", (req, res) => {
+    res.send({Project: "Test"});
+});
+
+app.listen(3001, function(){
+    console.log("express server is running on port 3001");
+})

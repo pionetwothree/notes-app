8564017901482118ab5a4579ff981e21fdeea7bd
pionetwothree <75122491+pionetwothree@users.noreply.github.com>
2021-04-27 15:23:05 +0200

    Create Notes1.jsx

---
+++ frontend/src/components/Notes1.jsx
@@ -0,0 +1,75 @@
+import React from 'react';
+
+//const app = Realm.App.getApp("<Your App ID>"); // replace this with your App ID
+
+export default class Notes1 extends React.Component {
+
+  constructor(props) {
+    super();
+    this.state = {
+      time: '',
+      loading: true,
+      description: ([{
+        //_id: '',
+        //title: '',
+        content: '',}])
+    };
+   
+/** 
+    state = {
+      loading: true,
+      description: ([{
+        //_id: '',
+        //title: '',
+        content: '',}])
+      };
+*/  
+
+    componentDidMount(() => {
+      const url = '/notes';
+      fetch(url).then((response) => {
+      const data = response.json() //response.json()
+      },
+      this.setState({ description: data, loading: false }),
+      this.setState({currentDateTime: Date().toLocaleString()}),
+      console.warn(data) 
+        //localStorage.setItem("notes1",JSON.stringify(data))
+      )}
+    )};
+  
+    
+    render() {
+      if (this.state.loading) {
+        return <div>loading...</div>;
+      }
+      if (!this.state.description) {
+        return <div>Error</div>;
+      }
+    }   
+    }
+
+      return (
+          <div className='container'>
+          <div className='center'>  
+          <button onClick={() => this.componentDidMount(), this.updateTime() } className="btn btn-lg btn-info">Update</button>  
+            {this.state.description.map(note => 
+                <div>
+                    <h1>{note.title}</h1>
+                    <p>{note.content}</p>
+                </div>
+            )}
+            <p>Time: { this.state.time } </p>
+            </div>
+
+      </div>
+      );
+    }
+  }
+
+  
+//Web Server for Chrome Extension in Google Chrome Browser verwenden für Bilder aus lokalen Laufwerk einbinden 
+//--> ansonsten Fehlermeldung
+//http://127.0.0.1:8888 und Ordner auswählen im Extension Tool
+
+
+//<h1>{note.title}</h1>
